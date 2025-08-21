f38aff4d286385134811daa13360ae37f6c4d1d5
djcolecarter-ship-it <djcolecarter@gmail.com>
2025-08-21 11:50:17 -0500

    Add files via upload

--- holdings_finder.py
+++ holdings_finder.py
@@ -34,19 +34,20 @@ if st.button("Find Holdings"):
                 for index, row in ria_data_chunk.iterrows():
                     product_name = row["NAMEOFISSUER"]
                     cusip = row["CUSIP"]
+                    security_info = row["TITLEOFCLASS"]
                     value = row["VALUE"]
                     formatted_value = f"${float(value):,}" if value else "N/A"
                     holdings.append({
                         "RIA Name": ria_name,
                         "Holding Name": product_name,
                         "CUSIP": cusip,
-                        "Dollar Amount": formatted_value,
-                        "Accession Number": accession_number
+                        "Security Info": security_info,
+                        "Dollar Amount": formatted_value
                     })
 
     results = pd.DataFrame(holdings)
     if not results.empty:
         st.write(f"Holdings for CRD Number {crd_number}:")
-        st.dataframe(results[["RIA Name", "Holding Name", "CUSIP", "Dollar Amount", "Accession Number"]])
+        st.dataframe(results[["RIA Name", "Holding Name", "CUSIP", "Security Info", "Dollar Amount"]])
     else:
         st.write(f"No holdings found for CRD Number {crd_number}.")
